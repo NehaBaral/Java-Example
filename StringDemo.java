1778ddae68a5f5ddf66e64ea038713560d46551a
NehaBaral <nehabaral011@gmail.com>
2018-07-30 08:10:11 +0545

    Demo

---
+++ StringDemo.java
@@ -0,0 +1,22 @@
+public class StringDemo{
+public static void main(String[] args){
+ String name="Neha Bhandari ";
+ //NEHA BARAL
+ //int i=10;
+ //String intToString=String.valueOf(i);
+ //String intToString=i.toString();
+ //System.out.println(intToString);
+ //Integer rollNo = 1;
+ //String s = rollNo.toString();
+ //System.out.println(s);
+// System.out.println(name.toUpperCase());
+ //System.out.println(name.toLowerCase());
+ //System.out.println(name.length());
+ //System.out.println("bt:"+name.length());
+ //String s1 = name.trim();
+ //System.out.println("at:"+s1.length());
+ System.out.println(name.startsWith("Neha"));
+ System.out.println(name.endsWith("z"));
+ 
+}
+}

2561aa7c879322853a48e31cd73cbf54264460dc
NehaBaral <nehabaral011@gmail.com>
2018-07-30 08:11:44 +0545

    Converter

---
+++ Converter.java
@@ -0,0 +1,25 @@
+public class Converter{
+  public static void main(String[] args){
+//String a= "abc123";
+	//int c=Integer.parseInt(a);
+	//System.out.println(c);
+	//String s=a.substring(0,2);
+	//System.out.println(s);
+	//String b=a.substring(3,6);
+	//System.out.println(b);
+	String a="Lata poudel neha siwakoti";
+	String s=a.substring(0,3);
+	System.out.println(s);sss
+	String b=a.substring(5,10);
+	System.out.println(b);
+	String c=a.substring(11,14);
+	System.out.println(c);
+	String d=a.substring(16,23);
+	System.out.println(s);
+	String s=a.substring(0,3);
+	System.out.println(s);
+	
+	
+	
+  }
+}

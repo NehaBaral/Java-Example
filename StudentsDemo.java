02b1216c4e6dd7d818f80892052d73c78ef34345
NehaBaral <nehabaral011@gmail.com>
2018-07-30 07:59:24 +0545

    New program

---
+++ StudentsDemo.java
@@ -0,0 +1,10 @@
+public class StudentsDemo{
+ public static void main(String[] args){
+   Students s=new Students("Chabahil");
+   System.out.println("Name: "+s.getName());
+   System.out.println("Roll no: "+s.getRoll());
+   System.out.println("Address: "+s.getAddress());
+   System.out.println("Phone num: "+s.getPhnum());
+ }
+ 
+}

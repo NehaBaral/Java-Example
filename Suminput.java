784b7b7b3535ba3699ccf4358bf9c93d46ed7aaf
NehaBaral <nehabaral011@gmail.com>
2018-07-30 08:07:17 +0545

    Sum

---
+++ Suminput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Suminput{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(input.in);
+	System.out.println("Enter first number:  ");
+	int a=Integer.parseInt(sc.nextLine());
+	System.out.println("Enter Second number: ");
+	int b=Integer.parseInt(sc.nextLine());
+	}
+	}
+	

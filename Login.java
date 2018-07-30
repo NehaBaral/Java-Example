4061d5c827116c587def04dc15629f3bdc942210
NehaBaral <nehabaral011@gmail.com>
2018-07-30 08:01:24 +0545

    Login page

---
+++ Login.java
@@ -0,0 +1,19 @@
+public class Login{
+  private String username="Neha";
+  private String password="12345";
+  
+  public Login(){
+  }
+  public String getUsername(){
+   return this.username;
+  }
+ public void setUsername(String un){
+  this.username=un;
+}
+ public String getPassword(){
+   return this.password;
+  }
+ // public void setPassword(String pw){
+  //this.password=pw;
+  //}
+}

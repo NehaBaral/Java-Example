6a6790bbc1e1ad30d256b14a003a29629c0393a7
NehaBaral <nehabaral011@gmail.com>
2018-07-30 06:34:32 +0545

    Sum program

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Sum{
+public static void main(String[] args){
+  int a=10;
+  int b=15;
+  int c=a+b;
+  System.out.println("Output of "+a+" and " +b+ " is " +c);
+  }
+  }
+     

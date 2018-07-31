f51a4a5f54b4c5ac6a8a0e19fb505aae9e62a079
NehaBaral <nehabaral011@gmail.com>
2018-07-31 06:46:42 +0545

    Sums

---
+++ Sums.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Sum{
+public static void main(String() args{
+  System.out.println("Enter first number: ");
+  int a=integer.parseInt(sc.nextLine());
+  System.out.println("Enter second line: ");
+  int b=integer.parseInt(sc.nextLine());
+  int c=a+b;
+  System.out.println("Output is "+c);
+  }
+  }
+  

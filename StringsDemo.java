caf90b67c61406d7eb5bd731cbef7cfd9b73a5c8
NehaBaral <nehabaral011@gmail.com>
2018-07-31 07:05:43 +0545

    Employee detail

---
+++ StringsDemo.java
@@ -0,0 +1,10 @@
+public class StringsDemo{
+ public static void main (String[] args){
+   String name = "Neha Bhandari Siwakoti";
+   //char c = name.charAt(5);
+   //String s = name.substring(5);
+   String s=name.replace("Siwakoti","Baral");
+   //System.out.println(c);
+   System.out.println(s);
+ }
+}
